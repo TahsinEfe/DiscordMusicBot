2c26ba1a22f362d8840d8ef456e393d846d654f1
TahsinEfe <tahsinefeyilmaz@hotmail.com>
2025-08-07 14:40:04 +0300

    initial commit

---
+++ DiscordMusicBot.cpp
@@ -0,0 +1,12 @@
+﻿// DiscordMusicBot.cpp : Defines the entry point for the application.
+//
+
+#include "DiscordMusicBot.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
